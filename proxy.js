8fe37e10d4140b7aa180e2e50e1827f6965429b9
Jason Stillwell <jstillwell@zendesk.com>
2017-06-22 00:31:58 -0700

    .

--- proxy.js
+++ proxy.js
@@ -1,26 +1,48 @@
 var lodash = require('lodash');
-var proxy = require('udp-proxy');
+var udpProxy = require('udp-proxy');
+var tcpProxy = require('tcp-proxy');
 
 var UDP_PORTS = [18481, 38217];
 var TCP_PORTS_LOW = 38216;
 var TCP_PORTS_HIGH = 38230;
 var TCP_PORTS = lodash.range(TCP_PORTS_LOW, TCP_PORTS_HIGH + 1);
+console.log("tcp ports " + TCP_PORTS);
 
-var options = {
-	address: '127.0.0.1',
-	port: 53,
-	ipv6: true,
-	localaddress: '0.0.0.0',
-	localport: 53535,
-	localipv6: false,
-	proxyaddress: '::0',
-	timeOutTime: 10000
-};
+var vridge_host = '64.62.255.87';
+var local_host = '0.0.0.0';
 
-var server = proxy.createServer(options);
+var udp_servers = [];
+UDP_PORTS.forEach(function (port) {
 
-// 'message' is emitted when the server gets a message
-server.on('message', function (message, sender) {
-	console.log('message from ' + sender.address + ':' + sender.port);
+	var options = {
+		address: vridge_host,
+		port: port,
+		localaddress: local_host,
+		localport: port
+	};
+
+	var server = udpProxy.createServer(options);
+	udp_servers.push(server);
+
+	// 'message' is emitted when the server gets a message
+	server.on('message', function (message, sender) {
+		console.log('message from ' + sender.address + ':' + sender.port);
+});
+});
+
+
+var tcp_servers = [];
+TCP_PORTS.forEach(function(port) {
+
+	var server = tcpProxy.createServer({
+		target: {
+			host: vridge_host,
+			port: port
+	}
+	});
+
+	server.listen(port);
+
+	tcp_servers.push(server);
 });
 
